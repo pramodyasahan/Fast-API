23080a940d143590b98a9f2537f6ae71d5d26ef9
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2025-01-31 21:15:35 +0530

    feat: create getting user method and simple tags

--- blog/main.py
+++ blog/main.py
@@ -17,7 +17,7 @@ def get_db():
         db.close()
 
 
-@app.post('/blog', status_code=201)
+@app.post('/blog', status_code=201, tags=['Blogs'])
 def create(request: schema.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -26,7 +26,7 @@ def create(request: schema.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.delete('/blog/{id}', status_code=204)
+@app.delete('/blog/{id}', status_code=204, tags=['Blogs'])
 def destroy(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -37,7 +37,7 @@ def destroy(id: int, db: Session = Depends(get_db)):
     return blog
 
 
-@app.put('/blog/{id}', status_code=202)
+@app.put('/blog/{id}', status_code=202, tags=['Blogs'])
 def update(id, request: schema.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
 
@@ -50,13 +50,13 @@ def update(id, request: schema.Blog, db: Session = Depends(get_db)):
     return "Updated Successfully"
 
 
-@app.get('/blog')
+@app.get('/blog', tags=['Blogs'])
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{blog_id}', response_model=schema.ShowBlog)
+@app.get('/blog/{blog_id}', response_model=schema.ShowBlog, tags=['Blogs'])
 def show(blog_id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
@@ -68,7 +68,7 @@ def show(blog_id: int, response: Response, db: Session = Depends(get_db)):
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@app.post("/user", response_model=schema.ShowUser)
+@app.post("/user", response_model=schema.ShowUser, tags=['Users'])
 def create_user(request: schema.User, db: Session = Depends(get_db)):
     hashed_password = pwd_context.hash(request.password)
     new_user = models.User(name=request.name, email=request.email, password=hashed_password)
@@ -79,3 +79,9 @@ def create_user(request: schema.User, db: Session = Depends(get_db)):
     return new_user
 
 
+@app.get('/user/{id}', response_model=schema.ShowUser, tags=['Users'])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail=f'User with id:{id} does not exist')
+    return user
