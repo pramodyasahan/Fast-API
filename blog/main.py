26768232806e9ada18123fc450a1f8d6928680a7
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2025-01-31 16:30:19 +0530

    feat: create simple CRUD operation and integrate SQL lite as database

---
+++ blog/main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from . import schema, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=201)
+def create(request: schema.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.delete('/blog/{id}', status_code=204)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=404, detail='Blog does not exist')
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return blog
+
+
+@app.put('/blog/{id}', status_code=202)
+def update(id, request: schema.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=404, detail='Blog does not exist')
+    blog.update(
+        {"title": request.title, "body": request.body}, synchronize_session=False)
+
+    db.commit()
+    return "Updated Successfully"
+
+
+@app.get('/blog')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{blog_id}')
+def show(blog_id: int, response: Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Blog not found with id: {blog_id}"}
+    return blog
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get("/")
 def index():
     return {"Hello": "World"}
 
 
-@app.get("/about")
-def about():
-    return {"about": "About Us"}
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    return {"data": f"Blog is created as {request.title}"}
