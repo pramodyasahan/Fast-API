cf3ea29671566416ab066abc984856f9cd0abe79
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2025-01-31 20:56:24 +0530

    feat: Add user creation endpoint and user schema models

--- blog/main.py
+++ blog/main.py
@@ -55,10 +55,20 @@ def get_all(db: Session = Depends(get_db)):
     return blogs
 
 
-@app.get('/blog/{blog_id}')
+@app.get('/blog/{blog_id}', response_model=schema.ShowBlog)
 def show(blog_id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"detail": f"Blog not found with id: {blog_id}"}
     return blog
+
+
+@app.post("/user")
+def create_user(request: schema.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
